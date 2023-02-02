c62079b39ead4cd9e90e18a3d87ff769f6189941
CristianAGR <cgr72677@udelasalle.edu.mx>
2023-02-01 21:09:57 -0600

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+    
+    return (
+        <div>
+            <h1 className="text-4xl font-bold uppercase text-indigo-600"> Desde Header</h1>
+            <p> Hola Mundo </p>
+        </div>
+    )
+}
+
+export default Header
